91c30970570b691920c22d071e92625b62cd3024
Prince <128713671+ft-prince@users.noreply.github.com>
2026-01-24 23:42:12 +0530

    Change LOGOUT_REDIRECT_URL to root path

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/'
 
 EVENTSTREAM_ALLOW_ORIGIN = '*'  
